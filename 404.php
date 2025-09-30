afd6c04536e8158e6e66dcba25e562327bce0d95
esbportfolio <sullivan.em88@gmail.com>
2025-09-30 05:07:13 -0500

    404 Page
    feat: Add support for 404 not found page

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying pages (i.e. posts with the type 'page')
+ * (A page is considered a type of post in WordPress)
+ */
+
+declare(strict_types=1);
+
+get_header();
+
+?>
+            <div class="container">
+                <div class="page">
+                    <h1>Page Not Found</h1>
+                    <p>The page you requested could not be found.  Please try again.</p>
+                    <p><a href="<?php echo get_site_url(); ?>">Return Home</a></p>
+                </div>
+            </div>
+<?php
+
+get_footer();
