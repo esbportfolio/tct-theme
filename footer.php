dee76993618712aa16f2b4a2fafd1aff3126a04d
esbportfolio <sullivan.em88@gmail.com>
2026-02-23 18:11:13 -0600

    Add Credit
    feat: Add footer credit line with styles

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
         </main>
         <footer>
+                <div class="container">
+                        <div class="credit">Logo and branding by <a href="https://skyeshrader.com/" target="_blank" rel="noopener noreferrer">Skye Shrader</a></div>
+                </div>
                 <div class="flower-border"></div>
         </footer>
 <!-- Begin Wordpress footer -->
